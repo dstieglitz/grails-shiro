0f9623ed9ae7b353deda22e7a33aab9de1242f1b
dstieglitz <dstieglitz@stainlesscode.com>
2018-03-29 11:27:20 -0700

    version 1.2.3

--- shiro2/ShiroGrailsPlugin.groovy
+++ shiro2/ShiroGrailsPlugin.groovy
@@ -45,7 +45,7 @@ import javax.servlet.Filter
 
 class ShiroGrailsPlugin {
 
-    def version = "1.2.3-SNAPSHOT"
+    def version = "1.2.3"
     def grailsVersion = "1.2 > *"
     def author = "Peter Ledbrook"
     def authorEmail = "peter@cacoethes.co.uk"
